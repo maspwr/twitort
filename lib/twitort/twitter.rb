3a395c42349d277eef0c581f25822303d035027a
Mark Aaron Shirley <markashirley@gmail.com>
2012-05-20 21:04:48 -0700

    modify twitter module to get an arbitrary number of tweets

--- lib/twitort/twitter.rb
+++ lib/twitort/twitter.rb
@@ -3,17 +3,33 @@ require 'json'
 
 module Twitort
   module Twitter
-    URL = "https://api.twitter.com/1/statuses/user_timeline.json"
-    
-    def self.get_tweets(username)
-      url      = format_url(:screen_name => username)
-      response = open(url).readlines
+    URL             = "https://api.twitter.com/1/statuses/user_timeline.json"
+    DEFAULT_OPTIONS = {:count => 200}
 
-      JSON.parse(response[0])
+    
+    def self.get_tweets(username, p = DEFAULT_OPTIONS)
+      max_count  = 1000
+      max_id     = nil
+      response   = []
+      params     = p.merge(:screen_name => username)
+
+      (max_count / p[:count]).ceil.times do
+        params    = params.merge(:max_id => max_id) if max_id
+        url       = format_url(params)
+        response += JSON.parse(open(url).readlines[0])
+        max_id    = get_max_id(response)
+      end
+
+      response
     end
 
     private
-    
+
+    def self.get_max_id(response)
+      sorted = response.sort { |a,b| a["id"] <=> b["id"] }[0]
+      sorted and sorted["id"] - 1
+    end
+
     def self.format_url(options)
       # FIXME: Make sure query parameters are percent escaped
       URL + "?" + options.to_a.map { |a| a.join('=') }.join('&')
